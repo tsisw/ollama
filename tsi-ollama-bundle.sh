40847a38492f70a47b8b1341be929e55b0bb69bf
Ashish Trivedi <atrivedi@tsavoritesi.com>
2026-04-03 21:23:28 -0700

    @FIR-1655: Update to support SDK-r.0.3.0 This changes updates the tar bundle creation to use SDK-r.0.3.0

--- tsi-ollama-bundle.sh
+++ tsi-ollama-bundle.sh
@@ -460,7 +460,7 @@ resolve_paths() {
   local arch="$1"
 
   if [ -z "${MLIR_COMPILER_DIR_IN}" ]; then
-    MLIR_SDK_VERSION="${MLIR_SDK_VERSION:-/proj/rel/sw/sdk-r.0.2.12/${arch}}"
+    MLIR_SDK_VERSION="${MLIR_SDK_VERSION:-/proj/rel/sw/sdk-r.0.3.0/${arch}}"
     MLIR_COMPILER_DIR_IN="${MLIR_SDK_VERSION}/compiler"
   fi
   if [ -z "${TOOLBOX_DIR_IN}" ]; then
