aae0fcd4db55ba934edac5ee23bbd3abcf7348e7
Ashish Trivedi <atrivedi@tsavoritesi.com>
2026-04-06 12:52:17 -0700

    @FIR-1663: updated the code for sdk-r0.3.1 release

--- tsi-ollama-bundle.sh
+++ tsi-ollama-bundle.sh
@@ -460,7 +460,7 @@ resolve_paths() {
   local arch="$1"
 
   if [ -z "${MLIR_COMPILER_DIR_IN}" ]; then
-    MLIR_SDK_VERSION="${MLIR_SDK_VERSION:-/proj/rel/sw/sdk-r.0.3.0/${arch}}"
+    MLIR_SDK_VERSION="${MLIR_SDK_VERSION:-/proj/rel/sw/sdk-r.0.3.1/${arch}}"
     MLIR_COMPILER_DIR_IN="${MLIR_SDK_VERSION}/compiler"
   fi
   if [ -z "${TOOLBOX_DIR_IN}" ]; then
@@ -731,7 +731,7 @@ bundle_fpga() {
 
   log_info "creating tar bundle for fpga (${build_dir})"
 
-  local TSI_GGML_VERSION=0.2.12
+  local TSI_GGML_VERSION=0.3.1
   local TSI_GGML_BUNDLE_INSTALL_DIR=tsi-ggml
   local GGML_TSI_INSTALL_DIR=ggml-tsi-kernel
   local TSI_GGML_RELEASE_DIR=/proj/rel/sw/ggml
