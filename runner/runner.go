98ce7aecb820a72ba61fc517899f9afb6075bfee
Anoop Kapoor <akapoor@wspd0.tsavoritesi.net>
2026-04-10 22:29:44 -0700

    @FIR-1689 - Ollama: GEMMA Model not offloaded to OPU and not releasing the TXE

--- runner/runner.go
+++ runner/runner.go
@@ -1,11 +1,13 @@
 package runner
 
 import (
+        "fmt"
 	"github.com/ollama/ollama/runner/llamarunner"
 	"github.com/ollama/ollama/runner/ollamarunner"
 )
 
 func Execute(args []string) error {
+        fmt.Printf("RUNNER: Execute called, args=%v\n", args)
 	if args[0] == "runner" {
 		args = args[1:]
 	}
@@ -15,7 +17,9 @@ func Execute(args []string) error {
 		args = args[1:]
 		newRunner = true
 	}
+	newRunner = false
 
+       fmt.Printf("RUNNER: forcing llama-runner\n")
 	if newRunner {
 		return ollamarunner.Execute(args)
 	} else {
